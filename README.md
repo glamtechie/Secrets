b6acc2f1dab11e4bfaadd3c59cf31f8a9504ccf5
Pooja Naik <glamtechie@Poojas-MacBook-Air.local>
2015-03-02 13:45:50 -0800

    updated

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # Secrets
 Secrets is a simple web.py app for storing personal secrets.
+
+Files:
+1. schema.sql - Describes tables used for this api
+2. auth_model.py - Describes the model used for this api
+3. authentication.py - Describes server methods and runs server
+4. test.sh - Can be used to test the methods of the api
+
+Required downloads to run this api:
+1. Curl (you may choose to send the requests using any other too , but you will have to write your own test commands)
+
+Currently, the api is deployed on: https://poojanaik.webfactional.com/secrets_app/
+
+Required downloads to host your own server:
+1. MySQL
+2. Python 2.7 (and associated libraries)
+
+API Requests:
+
+POST /new:
+    Create new user with username and password. Enter password twice.
+    Suggested usage: curl -k -X POST "$ROOT_URL/new" --data "user=poojanaik&pwd1=tarrega&pwd2=tarrega"
+    Returns: Badrequest if username exists, passwords don't match or either parameters not specified and displays error message on the server. Otherwise returns success with a message on the server.
+
+POST /login:
+    Login user with username and password.
+    Suggested usage: curl -k -X POST --header "Authorization: Basic poojanaik:tarrega" "$ROOT_URL/login"
+    Returns: Success with a message on the server if successful. Badrequest or unauthorized with error message displayed on the server.
+
+/secrets:
+
+    GET /secrets/(.*):
+        Get all or specific secrets of user (Note: User has to be logged in)
+        Suggested usage: curl -k -X GET "$ROOT_URL/secrets/" for all secrets
+                                                             for secret with a specific ID
+        Returns: Json object with secrets and their ids if sucessful. Unauthorized if not logged in.
+
+    POST /secrets:
+        Add a secret (Note: User has to be logged in)
+        Suggested usage: curl -k -X POST "$ROOT_URL/secrets/" --data "Very secretive here..."
+        Returns: Json object with posted secret or unauthorized if not logged in.
+
+    PATCH /secrets/(.*):
+        Modify a secret by specifying an id (Note: User has to be logged in)
+        Suggested usage: curl -k -X PATCH "$ROOT_URL/secrets/$POST_ID"
+        Returns: Json object with modified secret. Badrequest if invalid id, unauthorized if not logged in.
+
+    DELETE /secrets/(.*):
+        Delete specific or all secrets of user(Note: User has to be logged in)
+        Suggested usage: curl -k -X DELETE "$ROOT_URL/secrets/$POST_ID" for a specific id
+                         curl -k -X DELETE "$ROOT_URL/secrets/" for all secrets
+        Returns: Success if successful and badrequest or unauthorized if ID is invalid or not logged in
+
+Suggested extensions:
+1. Sharing secrets
+2. Public secrets
+3. Organizing secrets
+
+
+
+
+
--- authentication.py
+++ authentication.py
@@ -16,15 +16,15 @@ urls=(
 class Secrets:
     def GET(self, secret_id):
         if not session_data["loggedin"]:
-          web.ctx.status = '401 Unauthorized'
-          return "You must log-in before asking for secrets"
+            web.ctx.status = '401 Unauthorized'
+            return "You must log-in before asking for secrets"
 
         username = session_data['username']
 
         if secret_id:
-          response = auth_model.get_secret(username, secret_id)
+            response = auth_model.get_secret(username, secret_id)
         else:
-          response = auth_model.get_secrets(username)
+            response = auth_model.get_secrets(username)
 
         return json.dumps(list(response))
 
@@ -32,15 +32,15 @@ class Secrets:
     # Ignores secret_id
     def POST(self, secret_id):
         if not session_data["loggedin"]:
-          web.ctx.status = '401 Unauthorized'
-          return "You must log-in before posting secrets"
-          
+            web.ctx.status = '401 Unauthorized'
+            return "You must log-in before posting secrets"
+
         username = session_data['username']
         secret = web.data()
         secret_id = auth_model.new_secrets(username, secret)
 
         if not secret_id or secret_id < 0:
-          raise web.InternalError (message="Coudln't save secret. Try again later.")
+            raise web.InternalError (message="Coudln't save secret. Try again later.")
 
         return json.dumps({"id": secret_id, "secret": secret})
 
@@ -49,17 +49,17 @@ class Secrets:
         if not session_data["loggedin"]:
             web.ctx.status = '401 Unauthorized'
             return "You must log-in before deleting secrets"
-        
+
         username = session_data['username']
-        
+
         if not secret_id or len(secret_id) == 0:
-		response = auth_model.delete_secrets (username)		
-	else:
-		response = auth_model.delete_secret (username, secret_id)
+            response = auth_model.delete_secrets (username)
+	   else:
+            response = auth_model.delete_secret (username, secret_id)
 
         if response > 0:
-          return "OK"
-       
+            return "OK"
+
         raise web.BadRequest (message="Couldn't delete secret. Ensure the secret ID is valid by calling GET /secrets")
 
     # Updates a secret
@@ -71,14 +71,14 @@ class Secrets:
         username = session_data['username']
 
         if not secret_id or len(secret_id) == 0:
-          raise web.BadRequest (message="Must supply a secret ID in the URL")
-       
-        # New secret
+            raise web.BadRequest (message="Must supply a secret ID in the URL")
+
+        # modified secret
         secret = web.data()
         response = auth_model.modify_secret (username, secret_id, secret)
 
-	if response > 0:
-		return json.dumps({"id": secret_id, "secret": secret})
+	    if response > 0:
+            return json.dumps({"id": secret_id, "secret": secret})
 
         raise web.BadRequest (message="Couldn't update secret. Ensure the secret ID is valid by calling GET /secrets")
 
@@ -87,18 +87,18 @@ class Login:
         auth_header = web.ctx.env.get('HTTP_AUTHORIZATION')
 
         if not auth_header:
-          raise web.BadRequest("Authorization header must be present.") 
-        
+            raise web.BadRequest("Authorization header must be present.")
+
         try:
-          credentials = re.sub('^Basic ', '', auth_header)
-          # Not doing base64 encoding/decoding for simplicity
-          username, password = credentials.split(":")
+            credentials = re.sub('^Basic ', '', auth_header)
+            # Not doing base64 encoding/decoding for simplicity
+            username, password = credentials.split(":")
         except:
-          raise web.BadRequest("Authorization header is malformed.")
+            raise web.BadRequest("Authorization header is malformed.")
 
         pwdhash = hashlib.md5(password).hexdigest()
         pwdstore = list(auth_model.get_pass(username))
-      
+
         if not pwdstore or len(pwdstore)==0 or pwdstore[0]['password'] != pwdhash:
             web.ctx.status = '401 Unauthorized'
             return "Incorrect username/password combination"
@@ -120,7 +120,7 @@ class New:
         PWD2_KEY='pwd2'
 
         if USER_KEY not in data or PWD1_KEY not in data or PWD2_KEY not in data:
-          raise web.BadRequest (message = "Must send user, pwd1 and pwd2 in url encoded format")
+            raise web.BadRequest (message = "Must send user, pwd1 and pwd2 in url encoded format")
 
         user=data['user'][0]
         pwd1=data['pwd1'][0]
